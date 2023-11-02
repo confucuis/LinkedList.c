3805090f6f420d11c0abb387a78461d5afa2d02b
confucuis <confucuis@github.com>
2023-11-02 14:50:50 +0800

    双向链表

--- src/dlist.h
+++ src/dlist.h
@@ -2,3 +2,15 @@
 // 双链表
 // 增删改查
 */
+
+#ifndef _DLIST_H_
+#define _DLIST_H_
+
+// 存储整形的节点
+typedef struct {
+    int num;
+    struct ListNode *prev = NULL;
+    struct ListNode *next = NULL;
+} ListNode;
+
+#endif
